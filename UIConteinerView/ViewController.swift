988110239c35f3f7c76a69b6c85b9458fa02e3c2
Beibarys <beibarys1927@gmail.com>
2021-04-30 15:49:06 +0600

    Initial commit

---
+++ UIConteinerView/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  UIConteinerView
+//
+//  Created by Бейбарыс Шагай on 3/18/21.
+//  Copyright © 2021 Бейбарыс Шагай. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBOutlet weak var imageCover: UIImageView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
+
+}
+
